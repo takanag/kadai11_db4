530bfa3036559a6f86551204f9bbdd2ee1d6366f
takanag <takanag@gmail.com>
2025-01-24 00:54:57 +0900

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -27,10 +27,10 @@ function h($str)
 function db_conn() 
 {
     try {
-        $db_name = 'takanag_db_gensen'; //データベース名
-        $db_id   = 'takanag_db_gensen'; //アカウント名
-        $db_pw   = '9628Taka'; //パスワード：MAMPは'root'
-        $db_host = 'mysql3104.db.sakura.ne.jp'; //DBホスト
+        $db_name = ''; //データベース名
+        $db_id   = '; //アカウント名
+        $db_pw   = ''; //パスワード
+        $db_host = ''; //DBホスト
         $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
         return $pdo; //関数の外でPDO関数を使う場合にreturn $pdoとする
     } catch (PDOException $e) {
